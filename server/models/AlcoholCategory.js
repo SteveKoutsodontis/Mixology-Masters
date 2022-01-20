10fc3702f1bbd2f2c0c7d790311ff0d5df0d8a9e
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-20 11:26:42 -0500

    added models started seeds

---
+++ server/models/AlcoholCategory.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const alcoholCategorySchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    }
+  });
+
+const AlcoholCategory = mongoose.model('AlcoholCategory', alcoholCategorySchema);
+
+module.exports = AlcoholCategory;
