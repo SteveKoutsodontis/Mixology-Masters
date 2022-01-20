6f333a9f0b3523cfe5f7ddf60333fa9fd3ce5dab
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-20 12:06:42 -0500

    added alcohol categories to cocktail kit

---
+++ server/models/Order.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+  purchaseDate: {
+    type: Date,
+    default: Date.now
+  },
+  eventDate: {
+      type: Date,
+      required: true
+  },
+  CocktailKit: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'CocktailKit'
+    }
+  ],
+  AddOnsZoom: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'AddOnsZoom'
+    }
+  ],
+  AddOnsFood: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'AddOnsFood'
+    }
+  ]
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
