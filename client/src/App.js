b7220ce26a6ac06a588036bc663794d218be7b55
Steve Koutsodontis <skoutsodontis@gmail.com>
2022-01-23 19:37:55 -0500

    Login and Logout functioning.

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,12 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import LandingPage from './pages/LandingPage';
 import Home from './pages/HomePage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Navigation from './components/Navigation/index.js';
 import Footer from './components/Footer'
-import Header from './components/Header'
-// import Cart from './Cart/index';
+// import Cart from './components/Cart';
 // import { StoreProvider } from './utils/GlobalState';
 // import Success from './pages/Success/index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -43,7 +41,6 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div>
-        <Header />
         <div className="content">
           <Router>
             <Navigation />
@@ -57,9 +54,9 @@ function App() {
               <Route exact path="/signup">
                 <Signup />
               </Route>
-              {/* <Route exact path="/Cart">
-                  <Cart/>
-                </Route> */}
+              <Route exact path="/Cart">
+                  {/* <Cart/> */}
+                </Route>
               {/* <Route exact path="/success" component={Success} /> */}
               {/* <Route exact path="/products/:id" component={Detail} /> */}
               {/* <Route component={NoMatch} /> */}
