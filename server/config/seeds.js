b7aa65e862ab67a135da7c6d3bbb33511a357656
Sulaimon Fedai <sfedai211@gmail.com>
2022-01-25 07:38:32 -0500

    seed.js updated with cocktails

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -31,103 +31,228 @@ db.once('open', async () => {
       quantity: 500
     },
     {
-      name: 'Canned Coffee',
-      description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
-      category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
-    },
-    {
-      name: 'Toilet Paper',
-      category: categories[1]._id,
-      description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
-      price: 7.99,
-      quantity: 20
-    },
-    {
-      name: 'Handmade Soap',
-      category: categories[1]._id,
-      description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
-      price: 3.99,
-      quantity: 50
-    },
-    {
-      name: 'Set of Wooden Spoons',
-      category: categories[1]._id,
-      description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
-      price: 14.99,
-      quantity: 100
-    },
-    {
-      name: 'Camera',
-      category: categories[2]._id,
-      description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
-    },
-    {
-      name: 'Tablet',
-      category: categories[2]._id,
-      description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
-    },
-    {
-      name: 'Tales at Bedtime',
-      category: categories[3]._id,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
-    },
-    {
-      name: 'Spinning Top',
-      category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
-    },
-    {
-      name: 'Set of Plastic Horses',
-      category: categories[5]._id,
-      description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
-    },
-    {
-      name: 'Teddy Bear',
-      category: categories[6]._id,
-      description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
-    },
-    {
-      name: 'Alphabet Blocks',
-      category: categories[4]._id,
-      description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
-    }
+      category: "Gin",
+      name: "Ruby Negroni",
+      image: "https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg",
+      ingredients: ["Gin, Campari, Tawny Port, Raspberry Syrup, Prosecco, Raspberry"],
+      price: 15.00,
+      description: "Tawny Ports are wines made from red grapes, that are aged in wooden barrels, exposing them to gradual oxidation and evaporation."
+
+  },
+  {
+      category: "Gin",
+      name: "Vesper Martini",
+      image: "https://www.thecocktaildb.com/images/media/drink/mtdxpa1504374514.jpg",
+      ingredients: [" Gin, 100 Proof Vodka, Kina Lillet"],
+      price: 15.00,
+      description: "The Vesper Martini was invented and named by fictional secret agent James Bond in the 1953 novel Casino Royale."
+
+  },
+  {
+      category: "Gin",
+      name: "Gin Berry",
+      image: "https://www.thecocktaildb.com/images/media/drink/xrbhz61504883702.jpg",
+      ingredients: ["Gin, Cranberry Juice,Lime Juice,"],
+      price: 15.00,
+      description: "Cranberry juice was first made in the 1600s."
+
+  },
+  {
+      category: "Gin",
+      name: "Gimlet",
+      image: "https://www.thecocktaildb.com/images/media/drink/3xgldt1513707271.jpg",
+      ingredients: ["Premium Gin, Lime Cordial"],
+      price: 15.00,
+      description: "The drink was named after British Royal Navy Surgeon General Sir Thomas D.Gimlette who allegedly introduced this drink as a means of inducing his messmates to take lime juice as an anti-scurvy medication."
+
+  }, {
+      category: "Gin",
+      name: "Singapore Slink",
+      image: "https://www.thecocktaildb.com/images/media/drink/7dozeg1582578095.jpg",
+      ingredients: ["Gin, Cherry Heering, Benedictine, Lemon Juice,  Orange Bitters,  Angostura Bitters,  Soda Water, Lemon slice."],
+      price: 15.00,
+      description: "The Singapore Sling is a cocktail that was developed sometime before 1915 by Ngiam Tong Boon, a bartender working at the Long Bar in Raffles Hotel Singapore."
+
+  },
+  {
+      category : "Rum",
+      name: "Mojito",
+      image: "https://www.thecocktaildb.com/images/media/drink/metwgh1606770327.jpg",
+      ingredients: ["White Rum, 8 Mint leaves, Sugar Syrup, Lime Juice, 2 Mint sprigs"],
+      price: 15.00,
+      description: "In the 2006 movie Miami Vice lead character Crockett is seen drinking a Mojito from the bartender in the very first scene."
+
+  },
+  {
+      category: "Rum",
+      name: "Daiquri",
+      image: "https://www.thecocktaildb.com/images/media/drink/mrz9091589574515.jpg",
+      ingredients: ["White Rum, bar spoons Sugar, Lime Juice, Lime slice"],
+      price: 15.00,
+      description: "Due to Roosevelt`s Good Neighbour policy (which opened up trade with Cuba, Latin America and the Carribean) rum became easily obtainable in the 1940S and this helped drive the popularity of the daiquiri in the US."
+
+  },
+  {
+      category: "Rum",
+      name: "Pina Conolada",
+      image: "https://www.thecocktaildb.com/images/media/drink/cpf4j51504371346.jpg",
+      ingredients: ["White Rum, Pineapple Juice, Coconut Cream, Cream, Pineapple Slice, pinch of Salt"],
+      price: 15.00,
+      description: "In 1978 the government declared the Pina Colada to be the official drink of Puerto Rico."
+
+  },
+  {
+      category: "Rum",
+      name: "Dark N Stormy",
+      image: "https://www.thecocktaildb.com/images/media/drink/t1tn0s1504374905.jpg",
+      ingredients: ["Dark Rum, 4 Lime quarters, bar spoons Brown Sugar, splash Ginger Beer, Lime wedge"],
+      price: 15.00,
+      description: "This drink gained popularity through the sailing community up and down the east coast of the USA, having been brought home by various sailors who visited Bermuda."
+
+  },
+  {
+      category: "Rum",
+      name: "Cuba Libre",
+      image: "https://www.thecocktaildb.com/images/media/drink/ck6d0p1504388696.jpg",
+      ingredients: ["Anejo Rum, 8 Lime wedges, Cola, Lime wedge"],
+      price: 15.00,
+      description: "In Russia, Cuba Libre without the lime juice is called Rock-n-Roll Star, after a popular song that features the recipe."
+
+  },
+  {
+      category: "Tequilla",
+      name: "Margaritta",
+      image: "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+      ingredients: ["Tequila, Triple Sec, Lime Juice, Simple Syrup, salt"],
+      price: 15.00,
+      description: "Margarita is the most common tequila-based cocktail in the world."
+
+  },
+  {
+      category: "Tequilla",
+      name: "Paloma",
+      image: "https://www.thecocktaildb.com/images/media/drink/samm5j1513706393.jpg",
+      ingredients: ["Tequila, Pink Grapefruit Juice, Sugar Syrup,  Lime Juice, Pink Grapefruit wedge"],
+      price: 15.00,
+      description: "The Paloma is the most popular tequila based cocktail in Mexico."
+
+  },
+  {
+      category: "Tequilla",
+      name: "El Diablo",
+      image:"https://www.socialandcocktail.co.uk/wp-content/uploads/2011/12/El-Diablo.jpg",
+      ingredients: ["Tequila, Crème de Cassis, Lime Juice, Ginger Beer"],
+      price: 15.00,
+      description: "The EL Diablo appeared in Trader Vics Book of Food and Drink as early as 1946."
+
+  },
+  {
+      category: "Tequilla",
+      name: "Tequilla Sunrise",
+      image: "https://www.thecocktaildb.com/images/media/drink/quqyqp1480879103.jpg",
+      ingredients: ["Tequila,Orange Juice, dash Grenadine Syrup, Lime wedge, Orange slice"],
+      price: 15.00,
+      description: "Tequila Sunrise, featuring Mel Gibson, holds a score of 44% on Rotten Tomatoes."
+
+  },
+  {
+      category: "Tequilla",
+      name: "Baja Gold",
+      image: "https://www.socialandcocktail.co.uk/wp-content/uploads/2011/12/Baja-Gold-2.jpg",
+      ingredients: ["Tequila,Triple Sec, Pineapple Juice, Lime Juice, Simple Syrup, Lime Wedge"],
+      price: 15.00,
+      description: "Each pineapple plant produces one pineapple fruit."
+
+  },
+  {
+      category: "Vodka",
+      name: "White Russian",
+      image: "https://www.thecocktaildb.com/images/media/drink/vsrupw1472405732.jpg",
+      ingredients: ["Vodka, Coffee Liqueur, Cream, Milk, Chocolate Powder, Coffee Bean"],
+      price: 15.00,
+      description: "The White Russian is the favourite drink of Jeffrey The Dude Lebowski, the main character of the 1998 film, The Big Lebowski."
+
+  },
+  {
+      category: "Vodka",
+      name: "Cosmopolitan",
+      image: "https://www.thecocktaildb.com/images/media/drink/kpsajh1504368362.jpg",
+      ingredients: ["Citrus Vodka,Triple Sec, Cranberry Juice, squeezed lemon, Sugar Syrup"],
+      price: 15.00,
+      description: "The Cosmopolitan gained significant popularity in the 1990s partly due to the fact that Sarah Jessica Parkers character, Carrie Bradshaw, in the hit televsion programme, Sex in the City, often ordered the drink."
+
+  },
+  {
+      category: "Vodka",
+      name: "French Martini",
+      image: "https://www.thecocktaildb.com/images/media/drink/clth721504373134.jpg",
+      ingredients: [" Vodka, Chambord, Pineapple Juice, Raspberries for Garnish"],
+      price: 15.00,
+      description: "The part that makes this a French martini is the Chambord raspberry liqueur."
+
+  },
+  {
+      category: "Vodka",
+      name: "Espresso Maritni",
+      image: "https://www.thecocktaildb.com/images/media/drink/n0sx531504372951.jpg",
+      ingredients: ["Vanilla Vodka, Kahlúa, Double Espresso, Sugar Syrup, 3 Coffee Beans"],
+      price: 15.00,
+      description: "Coffee is the second most traded commodity on earth."
+
+  },
+  {
+      category: "Vodka",
+      name: "Moscow Mule",
+      image: "https://www.thecocktaildb.com/images/media/drink/3pylqc1504370988.jpg",
+      ingredients: ["Vodka, Lime juice, Sugar Syrup, splash Ginger Beer"],
+      price: 15.00,
+      description: "The Moscow Mule was invented in 1941 John G Martin at a bar in Sunset Boulevard which was popular with celebrities."
+
+  },
+  {
+      catergory: "Whisky",
+      name: "Old Fashioned",
+      image: "https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg",
+      ingredients:  ["Bourbon, Angostura Bitters, Orange Bitters, Sugar, Orange peel"],
+      price: 15.00,
+      description: "The name references the combinations age, as it is perhaps the first drink to be called a cocktail."
+  },
+  {
+      category: "Whisky",
+      name: "Lynchberg Lemonade",
+      image: "RubyNegroni.jpeg",
+      ingredients: ["Tennessee Whiskey,Triple -Sec,Sour Mix,Lemon-Lime Soda"],
+      price: 15.00,
+      description: "Lynchburg , Tennesse, is best known as the location of the Jack Daniels distillery."
+
+  },
+  {
+      category: "Whisky",
+      name: "Mint Julep",
+      image: "https://www.thecocktaildb.com/images/media/drink/squyyq1439907312.jpg",
+      ingredients: ["Knob Creek Bourbon, Granulated Sugar, 8 Mint leaves, Mint sprigs"],
+      price: 15.00,
+      description: "Since 1938, the Mint Julep has been promoted in association with the Kentucky Derby."
+
+  },
+  {
+      category: "Whisky",
+      name: "Donald Sutherland",
+      image: "https://www.socialandcocktail.co.uk/wp-content/uploads/2011/12/Donald-Sutherland.jpg",
+      ingredients: ["Rye Whiskey, Drambuie"],
+      price: 15.00,
+      description: "Both Donald and his son Kiefer starred in the 1997 movie A Time to Kill"
+
+  },
+  {
+      category: "Whisky",
+      name: "Sazerac",
+      image: "https://www.thecocktaildb.com/images/media/drink/vvpxwy1439907208.jpg",
+      ingredients: ["Rye Whiskey, Absinthe, 3 dashes of Peychauds Bitters, Sugar Cube, Lemon Peel"],
+      price: 15.00,
+      description: "In The Curious Case of Benjamin Button when he meets with his father for drinks, his father orders a Sazerac."
+
+  }
   ]);
 
   console.log('products seeded');
