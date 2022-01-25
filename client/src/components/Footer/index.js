7304845a7047190911c6f9e8f5f9b061b1fa4567
Steve Koutsodontis <skoutsodontis@gmail.com>
2022-01-25 09:10:53 -0500

    Working on Nav bar footer logo and background picture. Started commenting out some CSS not being used to fixed styling issues

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import logo from "../images/pj_logo1.png";
 
 export default function Footer() {
     return (
-        <div>
-            <p>this is the footer</p>
-            {/* <img id='pjLogo' src={PjLogo} alt="pjLogo" /> */}
+        <div className="footer xl p-5">
+            <p>Concocted by Nat, Linda, Sulaimon, Joe and Steve.</p>
+            <a href="/Home">
+          <img src={logo} style={{width:50, marginTop: -7}} />
+          </a>
         </div>
     )
 }
