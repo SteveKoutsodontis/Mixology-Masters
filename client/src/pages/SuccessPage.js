0df9feedb1722855f68c51d38087cc92afe5fd72
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-26 11:03:07 -0500

    adding Steve changes to login/signup/cart

--- client/src/pages/SuccessPage.js
+++ client/src/pages/SuccessPage.js
@@ -1,25 +1,25 @@
 import React from "react";
-import Jumbotron from 'react-bootstrap/Jumbotron';
+// import Jumbotron from 'react-bootstrap/Jumbotron'
 import { useEffect } from "react";
 // import { Redirect } from 'react-router-dom';
-
+{/* <Button><Link to="/Success">Checkout</Link></Button> */}
 function SuccessPage() {
-  const redirect = () => {
+  // const redirect = () => {
     useEffect(() => {
       setTimeout(() => {
         window.location.assign('/Home');
       }, 3000)
     })
-  }
-  redirect()
+  // }
+  // redirect()
   return (
     <div>
-      <Jumbotron>
-        <h1>Success!</h1>
-        <p>
+      
+        <h1 className="success">Success!</h1>
+        <p className="funFont text-center">
           Your order has been successfully placed! Please give us 3-5 business days for your order to be processed and shipped!
         </p>
-      </Jumbotron>;
+     
     </div>
   );
 }
