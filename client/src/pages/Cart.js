e381091d4e740ca0f5a6ae5aa19ec2bdd8e1060f
Steve Koutsodontis <skoutsodontis@gmail.com>
2022-01-26 19:18:14 -0500

    started cleaning up

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,60 +5,45 @@ import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
 export const Cart = () => {
-//   const [removeCartItem, { error }] = useMutation(REMOVE_CART_ITEM);
-
-//   const removefromCart = async (event) => {
-//     event.preventDefault();
-// console.log ({props});
-//     try {
-//       const cartItem = await removeCartItem({
-//         variables: { product: props._id},
-//       });
-//       alert(`${props.name} removed from cart!`);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-
-  
+ 
   const { loading, data } = useQuery(QUERY_USER);
 
   const cartItems = data?.user.cartItems;
 
   function renderCards() {
     let itemsOfCart = []
-    for(let item of cartItems){
-       itemsOfCart.push (
-       <>
-        <Card className="cartItem">
-          <Card.Img variant="top" src={item.image} />
-          <Card.Body>
-            <Card.Title>{item.name}</Card.Title>
-            <Card.Text>{"$" + item.price}</Card.Text>
-            <Button variant="danger">Remove from cart</Button>
-          </Card.Body>
-        </Card>
+    for (let item of cartItems) {
+      itemsOfCart.push(
+        <>
+          <Card className="cartItem">
+            <Card.Img variant="top" src={item.image} />
+            <Card.Body>
+              <Card.Title>{item.name}</Card.Title>
+              <Card.Text>{"$" + item.price}</Card.Text>
+              <Button variant="danger">Remove from cart</Button>
+            </Card.Body>
+          </Card>
         </>
-       )
+      )
     };
     return itemsOfCart
   }
 
-  let total=0;
+  let total = 0;
 
   function renderList() {
     let listItems = []
-    for(let item of cartItems){
-      total+= item.price
-      listItems.push (
+    for (let item of cartItems) {
+      total += item.price
+      listItems.push(
         <>
-       <h3>{item.name}</h3>
-       <p>{"$" + item.price}</p>
-       <hr></hr>
+          <h3>{item.name}</h3>
+          <p>{"$" + item.price}</p>
+          <hr></hr>
         </>
-       )
-      };
-  total = parseFloat(total).toFixed(2)
+      )
+    };
+    total = parseFloat(total).toFixed(2)
     return listItems
   }
 
@@ -70,26 +55,22 @@ export const Cart = () => {
           <h1>Cart</h1>
         </Card.Header>
         <div className="col-9 cartItemsContainer">
-          {!loading ? renderCards(): <h2>Loading.....</h2>}
+          {!loading ? renderCards() : <h2>Loading.....</h2>}
         </div>
 
         <div className="col-3 cartList ">
           <div>
-          <div>
-          {!loading ? renderList(): <h2>Loading.....</h2>}
-          
-          </div>
-          <Card.Header className="center">
-            <h3>Total: {total}</h3>
-            <Link to="/Success"><Button> Checkout</Button></Link>
-          </Card.Header>
+            <div>
+              {!loading ? renderList() : <h2>Loading.....</h2>}
+
+            </div>
+            <Card.Header className="center">
+              <h3>Total: {total}</h3>
+              <Link to="/Success"><Button> Checkout</Button></Link>
+            </Card.Header>
           </div>
         </div>
-        {/* <Card.Footer className="center">
-          <h1>Drink Responsibly!!!</h1>
-        </Card.Footer> */}
       </div>
-      <Footer />
     </div>
   );
 };
